db2487631f4fd9162b5a352782821e816e6d7b54
Wiktoria <wiktoria.janska@outlook.com>
2023-10-08 14:53:47 +0200

    initail commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+
   );
 }
 
---
+++ src/components/layout/SignedOutLinks.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { NavLink } from "react-router-dom";
+
+const SignedOutLinks = () => {
+    return (
+        <ul className="right">
+            <li><NavLink to='/signup'>Zarejestruj się</NavLink></li>
+            <li><NavLink to='/signin'>Zaloguj się</NavLink></li>
+        </ul>
+    );
+}
+ 
+export default SignedOutLinks;
