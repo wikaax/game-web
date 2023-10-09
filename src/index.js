2a38eaffd2fa6c815b99dcce3f9a02f64e1361a8
Wiktoria <wiktoria.janska@outlook.com>
2023-10-09 23:54:31 +0200

    small improvement

--- src/index.js
+++ src/index.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { configureStore, applyMiddleware } from 'redux';
+import { legacy_createStore, applyMiddleware } from 'redux';
 import rootReducer from './store/reduces/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
-const store = configureStore(rootReducer, applyMiddleware(thunk)); // legacy_createStore as createStore if this does not work
+const store = legacy_createStore(rootReducer, applyMiddleware(thunk)); // legacy_createStore as createStore if this does not work
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-registerServiceWorker();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// registerServiceWorker();
 
 
